3229dcb650bf919211caf73e30d1940d334e2de8
Vishal Gupta <vguptakota1998@gmail.com>
2025-11-13 10:42:25 +0530

    feat: add basic example setup

---
+++ FacebookExample/Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FacebookExample
+//
+//  Created by Vishal Gupta on 12/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FacebookExample/Example/FacebookExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookExampleApp.swift
+//  FacebookExample
+//
+//  Created by Vishal Gupta on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FacebookExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
