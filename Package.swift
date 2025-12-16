b0100c73e0b1e14747fdc527e7f3f0a84335d4f9
Satheesh Kannan <satheesh@rudderstack.com>
2025-12-16 19:14:12 +0530

    chore: update swift sdk version in package.swift file

--- Package.swift
+++ Package.swift
@@ -19,8 +19,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/facebook/facebook-ios-sdk", .upToNextMajor(from: "18.0.0")),
-        // todo: update the rudder-sdk-swift dependency after stable release
-        .package(url: "https://github.com/rudderlabs/rudder-sdk-swift.git", branch: "feat/sdk-502-make-standard-integration-public")
+        .package(url: "https://github.com/rudderlabs/rudder-sdk-swift.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         .target(
